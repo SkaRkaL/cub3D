42116f031ad56c8329503d5e515b556fa1c1a69c
Saad Karkal <Karkalsaad@gmail.com>
2024-01-15 17:23:37 +0100

    Libft

---
+++ libft/ft_strtok.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strtok.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 17:05:47 by sakarkal          #+#    #+#             */
+/*   Updated: 2024/01/12 01:42:39 by sakarkal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3D.h"
+
+char	*ft_strtok(char *str, char *sep)
+{
+	static char	*rem;
+	char		*ret;
+	int			length;
+
+	if (str)
+		rem = str;
+	if (!rem)
+		return (NULL);
+	while (*rem && ft_strchr(sep, *rem))
+		rem++;
+	length = 0;
+	while (rem[length] && !ft_strchr(sep, rem[length]))
+		length++;
+	if (!length)
+		return (NULL);
+	ret = ft_substr(rem, 0, length);
+	rem += length;
+	while (*rem && ft_strchr(sep, *rem))
+		rem++;
+	return (ret);
+}
---
+++ libft/ft_strtrim.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strtrim.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakarkal <sakarkal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 16:59:54 by sakarkal          #+#    #+#             */
+/*   Updated: 2024/01/12 01:48:57 by sakarkal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3D.h"
+
+int	ft_strlcpy(char *dst, const char *src, int dstsize)
+{
+	int	i;
+	int	x;
+
+	x = ft_strlen(src);
+	i = 0;
+	if (dstsize != 0)
+	{
+		while (src[i] && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	return (x);
+}
+
+char	*ft_substr(char const *s, unsigned int start, unsigned int len)
+{
+	char		*str;
+	size_t		i;
+
+	if (!s)
+		return (NULL);
+	if (!len || start >= (unsigned int)ft_strlen(s))
+		return (ft_strdup(""));
+	i = ft_strlen(s) - start;
+	if (i > len)
+		i = len;
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s + start, i + 1);
+	return (str);
+}
+
+static char	*ft_sdup(const char *s)
+{
+	int		i;
+	int		y;
+	char	*ptr;
+
+	i = 0;
+	y = ft_strlen(s);
+	ptr = malloc(y + 1 * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	while (s[i])
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strtrim(char const *s, char const *set)
+{
+	int		strt;
+	int		end;
+	char	*ptr;
+
+	strt = 0;
+	if (s == NULL)
+		return (NULL);
+	if (s[0] == '\0')
+		return (ft_sdup(""));
+	if (set == NULL)
+		return (ft_sdup(s));
+	while (ft_strchr(set, s[strt]) && s[strt])
+		strt++;
+	end = ft_strlen(s);
+	while (ft_strchr(set, s[end]) && end != 0)
+		end--;
+	if (end < strt)
+		return (ft_sdup(""));
+	ptr = ft_substr(s, strt, end - strt + 1);
+	return (ptr);
+}
